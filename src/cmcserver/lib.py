c0ac4cb2a74e65a60fc810ca36a31de86c2aed3b
Cody Finegan <44886+codyfinegan@users.noreply.github.com>
2023-05-07 19:24:08 +1200

    Adding in the start/stop commands

--- src/cmcserver/lib.py
+++ src/cmcserver/lib.py
@@ -1,13 +1,12 @@
-try:
-    import tomllib
-except ModuleNotFoundError:
-    import tomli as tomllib
-
+import json
+import subprocess
+import time
 from pathlib import Path
 from typing import List
 
 import click
-from mctools import RCONClient  # Import the RCONClient
+import tomlkit
+from mctools import PINGClient, RCONClient
 
 
 def debug_echo(debug: bool, message: str) -> None:
@@ -15,11 +14,67 @@ def debug_echo(debug: bool, message: str) -> None:
         click.echo(message)
 
 
+def default_config() -> dict:
+    return {
+        "server": {
+            "host": "127.0.0.1",
+            "rcon_port": 25575,
+            "rcon_password": "",
+            "query_port": 25565,
+        },
+        "context_dir": "/path/to/game/folder",
+        "startup": "startup_script.sh",
+        "boot_pause": 15,
+    }
+
+
+def default_config_toml() -> tomlkit.document:
+    cfg = default_config()
+    doc = tomlkit.document()
+    doc.add(tomlkit.comment("Configuration for cmcserver command"))
+    doc.add(tomlkit.nl())
+
+    doc.add("context_dir", cfg["context_dir"])
+    doc["context_dir"].comment(
+        "The path to the folder containing the game / startup script.",
+    )
+    doc.add("startup", cfg["startup"])
+    doc["startup"].comment(
+        "The actual shell script or whatever that will invoke the server.",
+    )
+    doc.add("boot_pause", cfg["boot_pause"])
+    doc["boot_pause"].comment(
+        "How long to wait for the server to start before continuing. Increase this if needed.",
+    )
+    doc.add(tomlkit.nl())
+
+    table = tomlkit.table()
+    table.add(
+        tomlkit.comment(
+            "Options related to server access, check your server.properties file.",
+        ),
+    )
+    table.add(
+        tomlkit.comment(
+            "If running on the same server, make sure ports are blocked exernally",
+        ),
+    )
+    table.add(tomlkit.comment("so rcon and query cannot be accessed externally."))
+    table.add("host", cfg["server"]["host"])
+    table.add("rcon_password", cfg["server"]["rcon_password"])
+    table.add("rcon_port", cfg["server"]["rcon_port"])
+    table.add("query_port", cfg["server"]["query_port"])
+    table["rcon_password"].comment("Password set in server.properties. Make it good.")
+    doc.add("server", table)
+
+    return doc
+
+
 class Config:
     """Config object parses and holds the config read from file."""
 
     def __init__(self, data: dict, debug) -> None:
-        self.data = data
+        self.data = default_config() | data
         self.debug = debug
         pass
 
@@ -28,9 +83,6 @@ class Config:
 
     def get(self, key):
         return self.data[key]
-
-    def server(self, key):
-        return self.data["server"][key]
 
     @classmethod
     def load(cls, config_file: str, debug: bool):
@@ -55,11 +107,12 @@ class Config:
             )
             raise SystemExit
 
-        data = tomllib.loads(config_file.read_text(encoding="utf-8"))
-        return cls(data, debug)
+        with open(config_file, mode="rt", encoding="utf-8") as fp:
+            data = tomlkit.load(fp)
+            return cls(data, debug)
 
     @classmethod
-    def write(cls, config_file: str):
+    def write(cls, config_file: str, force: bool):
         """Create a default config file in the provided path.
 
         Args:
@@ -67,37 +120,152 @@ class Config:
         """
         if config_file.exists():
             click.echo(f"Config is read from: {config_file}")
-            click.echo(
-                "Config file already exists. Please delete it if you want to init again.",
-            )
-            return
+            if not force:
+                click.echo(
+                    "Config file already exists. Please delete it if you want to init again.",
+                )
+                return
 
         config_file = Path(config_file)
-        with open(config_file, "w") as f:
-            f.write("# Default Configuration")
+        doc = default_config_toml()
+
+        with open(config_file, mode="wt", encoding="utf-8") as fp:
+            tomlkit.dump(doc, fp)
             click.echo(f"Created config file {config_file}")
 
 
 class Communicator:
+    """Communication class to a RCON service"""
+
     def __init__(self, cfg: Config) -> None:
         self.cfg = cfg
+        self.client = None
 
-    def _send(self, commands: List[str]):
-        host = self.cfg.server("host")
-        port = self.cfg.server("port")
-        password = self.cfg.server("password")
+    def ping_server(self, ping: bool = True):
+        try:
+            debug_echo(self.cfg.debug, "Attempting to ping the server...")
+            host = self.cfg.data["server"]["host"]
+            port = self.cfg.data["server"]["query_port"]
 
+            ping_q = PINGClient(host=host, port=port, format_method=PINGClient.REMOVE)
+
+            if not ping:
+                return ping_q.get_stats()
+
+            ping_q.ping()
+            return True
+        except IndexError:
+            # Happens when we ping too soon
+            return False
+        except ConnectionError:
+            return False
+
+    def _get_client(self):
+        if type(self.client) == RCONClient and self.client.is_authenticated:
+            return self.client
+
+        host = self.cfg.data["server"]["host"]
+        port = self.cfg.data["server"]["rcon_port"]
+        password = self.cfg.data["server"]["rcon_password"]
+
+        self.client = RCONClient(host, port, format_method=RCONClient.REMOVE)
+        if not self.client.login(password):
+            debug_echo(self.cfg.debug, "Login refused")
+            raise ConnectionRefusedError
+
+        debug_echo(self.cfg.debug, "Login complete")
+        return self.client
+
+    def _raw_send(self, commands: List[str]):
+        r = list()
+        rcon = self._get_client()
+
+        for command in commands:
+            debug_echo(self.cfg.debug, f"Command send: {command}")
+            r.append((command, rcon.command(command)))
+
+        return r
+
+    def send(self, commands: List[str]):
+        if type(commands) == str:
+            commands = [commands]
         try:
             debug_echo(self.cfg.debug, "Starting to login...")
-            rcon = RCONClient(host, port)
-            if rcon.login(password):
-                for command in commands:
-                    debug_echo(self.cfg.debug, f"Command send: {command}")
-                    resp = rcon.command(command)
-                    click.echo(resp)
+            return self._raw_send(commands)
         except ConnectionRefusedError as e:
             debug_echo(self.cfg.debug, str(e))
+            host = self.cfg.data["server"]["host"]
+            port = self.cfg.data["server"]["rcon_port"]
             raise click.UsageError(f"Unable to connect to server {host}:{port}")
 
-    def online(self):
-        self._send(["/list"])
+    def tell_all(
+        self,
+        message: str,
+        formatted: bool = False,
+        color: str = "gray",
+        italic: bool = True,
+        play_sound: bool = True,
+        sound: str = "ui.button.click",
+    ):
+        if not formatted:
+            message = {
+                "text": message,
+                "color": color,
+                "italic": italic,
+            }
+            message = json.dumps(message)
+
+        commands = [
+            f"/tellraw @a {message}",
+        ]
+        if play_sound:
+            commands.insert(0, f"playsound minecraft:{sound} voice @a")
+
+        debug_echo(self.cfg.debug, commands)
+        self.send(commands)
+
+
+class ToolLoader:
+    """Middleman just to make the communicator and config available to commands."""
+
+    def setup(self, config: Config):
+        self.config = config
+        self.communicator = Communicator(config)
+
+
+class ScreenManager:
+    """Simple commands for managing screen calls."""
+
+    @staticmethod
+    def exists() -> bool:
+        command = ["screen", "-list", "mcs"]
+        retcode = subprocess.call(command, stdout=subprocess.PIPE)
+        if retcode == 0:
+            return True
+        return False
+
+    @staticmethod
+    def start(debug: bool, startup_script: Path, context_path: Path, sleep: int):
+        command = ["screen", "-dmS", "mcs", "sh", "-c", startup_script]
+
+        if debug:
+            click.echo("Command: " + " ".join(command))
+            return False
+
+        process = subprocess.Popen(
+            command,
+            cwd=context_path,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+        )
+        process.wait()
+
+        time.sleep(sleep)
+
+        # Check, is our screen running?
+        if not ScreenManager.exists():
+            click.echo(f"No screen session was detected after {sleep} seconds.")
+            return False
+
+        # Screen has loaded, we don't know about the server but screen is running
+        return True
--- src/cmcserver/main.py
+++ src/cmcserver/main.py
@@ -1,30 +1,29 @@
 import os
+import time as timelib
 from pathlib import Path
 
 import click
 
-from .lib import Communicator, Config
+from .lib import Config, ScreenManager, ToolLoader
 
-
-def debug_echo(debug: bool, message: str) -> None:
-    if debug:
-        click.echo(message)
+pass_loader = click.make_pass_decorator(ToolLoader, ensure=True)
 
 
 @click.group(invoke_without_command=True)
 @click.option("--debug/--no-debug", default=False)
 @click.option("--init", is_flag=True, default=False)
+@click.option("--force", is_flag=True, default=False)
 @click.pass_context
-def cli(ctx, debug, init):
+def cli(ctx, debug, init, force):
     ctx.ensure_object(dict)
-    ctx.obj["DEBUG"] = debug
+    ctx.obj = ToolLoader()
 
     config_folder = os.environ.get("CMC_CONFIG", str(Path.home()))
     config_path = Path(config_folder).joinpath(".cmcconfig.toml")
 
     # Are we running init?
     if init:
-        Config.write(config_path)
+        Config.write(config_path, force)
         raise SystemExit
 
     if ctx.invoked_subcommand is None:
@@ -33,11 +32,111 @@ def cli(ctx, debug, init):
         ctx.exit()
 
     # Load the config option
-    ctx.obj["cfg"] = Config.load(config_path, debug)
-    ctx.obj["comm"] = Communicator(ctx.obj["cfg"])
+    ctx.obj.setup(Config.load(config_path, debug))
 
 
 @cli.command()
-@click.pass_context
-def online(context):
-    context.obj["comm"].online()
+@pass_loader
+def status(loader: ToolLoader):
+    """See the status of the server"""
+    x = loader.communicator.send(["list"])
+    print(x)
+
+
+@cli.command(name="server:stats")
+@pass_loader
+def ping(loader: ToolLoader):
+    print(loader.communicator.ping_server(ping=False))
+
+
+@cli.command(name="server:stop")
+@click.option(
+    "-t",
+    "--time",
+    default=5,
+    type=click.IntRange(0, 120),
+    help="How many seconds until shutdown?",
+)
+@click.argument("reason", type=str, default=None, required=False)
+@pass_loader
+def stop(loader: ToolLoader, time: int, reason: str):
+    """Stop the server"""
+    if not ScreenManager.exists() and not loader.communicator.ping_server():
+        click.echo("The server is not running.")
+        return
+
+    if not reason:
+        reason = ""
+    else:
+        reason = f" {reason}"
+
+    if time < 5:
+        time = 5
+
+    loader.communicator.tell_all(
+        f"The server is stopping in {time} seconds",
+        sound="block.bell.use",
+    )
+
+    if time > 5:
+        timelib.sleep(time - 5)
+
+    # 5 second countdown
+    for i in range(5, 0, -1):
+        loader.communicator.tell_all(f"In {i}...")
+        timelib.sleep(1)
+
+    loader.communicator.tell_all("Off we go!", sound="block.bell.use")
+    timelib.sleep(1)
+    loader.communicator.send(
+        [
+            "/kick @a Sever is stopping, check Discord for info",
+            "/stop",
+        ],
+    )
+
+
+@cli.command(name="server:start")
+@pass_loader
+def boot(loader: ToolLoader):
+    """Boot the server."""
+    if ScreenManager.exists():
+        click.echo("There is already a running server (or at least a screen session.)")
+        click.echo("Run `screen -ls mcs` to see the existing processes.")
+        return
+
+    context_path = Path(loader.config.get("context_dir"))
+    startup = Path(loader.config.get("startup"))
+    startup_script = context_path.joinpath(startup)
+
+    if not ScreenManager.start(
+        loader.config.debug,
+        startup_script,
+        context_path,
+        loader.config.get("boot_pause"),
+    ):
+        raise SystemExit
+
+    # Now we want to wait for Minecraft to be up and good
+    # We're going to check about 10 times with a pause between each
+    # And try to rcon
+    # If rcon is not configured then this might be a problem
+
+    loaded = False
+    for i in range(10):
+        # We want to check if our screen window is alive
+
+        # Are we alive?
+        click.echo("Checking if the server is up yet...")
+        if loader.communicator.ping_server():
+            loaded = True
+            break
+        click.echo("Not yet, waiting a few moments...")
+        timelib.sleep(5)
+
+    if loaded:
+        click.echo("Server has been loaded.")
+    else:
+        click.echo(
+            "Could not tell if the server loaded successfully, check it out. Make sure rcon/query has been configured",
+        )
